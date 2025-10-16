6ee9aefaa766e60e99686fc959d388880584d332
Ricardo Schalkwyk <schalkwykr24@gmail.com>
2025-10-16 07:16:39 +0200

    feat: added top games navigation

--- src/components/SideBarComp/Top.tsx
+++ src/components/SideBarComp/Top.tsx
@@ -3,28 +3,69 @@ import {
   GlobeAltIcon,
   TrophyIcon,
 } from "@heroicons/react/24/solid";
+import { useDispatch } from "react-redux";
+import { useNavigate } from "react-router-dom";
 
-import NavLink from "../NavLink";
+import Api from "../../api";
+import { setGames } from "../../store/games";
+import { GetResult } from "../../types";
 import Icon from "../Icon";
+import NavLink from "../NavLink";
 
 type Props = {
   name: string;
   icon: React.FC<React.SVGProps<SVGSVGElement>>;
+  query: string;
+  url: string;
 };
-const Top = () => {
-  const Options: Props[] = [
-    { name: "Best of the year", icon: TrophyIcon },
-    { name: "Popular in 2022", icon: ChartBarIcon },
-    { name: "All time best", icon: GlobeAltIcon },
-  ];
+
+const Options: Props[] = [
+  {
+    name: "Best of the year",
+    icon: TrophyIcon,
+    query: "lists/greatest?ordering=-added",
+    url: "/discover/best-of-the-year",
+  },
+  {
+    name: "Popular in 2024",
+    icon: ChartBarIcon,
+    query: "lists/greatest?year=2024",
+    url: "/discover/popular-in-2024",
+  },
+  {
+    name: "All time best",
+    icon: GlobeAltIcon,
+    query: "lists/popular?discover=true&",
+    url: "/discover/all-time-top",
+  },
+];
+
+export default function Top() {
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+
+  const handleFetch = async (item: Props) => {
+    try {
+      // Get input for the search
+      const { results, count } = await Api.get<GetResult>(
+        `/games/${item.query}&page_size=40&page=1`
+      );
+
+      dispatch(setGames({ results, count }));
+      navigate(item.url, { state: { query: item.query } });
+    } catch (error) {
+      console.log("Item not found");
+    }
+  };
 
   return (
     <div className="space-y-2">
       {Options.map((item, index) => (
         <NavLink
-          to={""}
+          to={item.url}
           key={index}
           className="group flex w-full items-center gap-2 border-solid text-lg font-thin"
+          onClick={() => handleFetch(item)}
         >
           <Icon
             icon={item.icon}
@@ -35,8 +76,4 @@ const Top = () => {
       ))}
     </div>
   );
-};
-
-Top.propTypes = {};
-
-export default Top;
+}
